c3bbb334c6905be71e18eb086919cf491795203f
IFREECAN <jasper.kinnay@gmmail.com>
2018-01-27 16:00:49 -0800

    data structure

--- scripts/views/music-view.js
+++ scripts/views/music-view.js
@@ -1,1 +1,107 @@
 'use strict';
+
+nightArray = [];
+activeArray = [];
+
+
+//All Night sounds
+{
+    _id: ObjectId(will.be.auto.generated )
+    title: 'Nightmood_category',
+    sound: 'Ocean1',
+    by: 'Artist1',
+    url: 'http://www.soundlocationOnYouTube.com',
+    // tags: ['ocean1', 'ocean2', 'ocean3'],
+    // likes: 100, 
+//     comments: [	
+//        {
+//           user:'user1',
+//           message: 'My first comment',
+//           dateCreated: new Date(2011,1,20,2,15),
+//           like: 0 
+//        },
+//        {
+//           user:'user2',
+//           message: 'My second comments',
+//           dateCreated: new Date(2011,1,25,7,45),
+//           like: 5
+//        }
+//     ]
+//  }
+
+ {
+    _id: ObjectId(will.be.auto.generated )
+    title: 'AcitvemoodCategory', 
+    sound: 'ocean2',
+    by: 'mixArtist',
+    url: 'http://www.soundlocationOnYouTube.com',
+    mylist: 'myActiveList',
+ }
+
+ {
+    _id: ObjectId(will.be.auto.generated )
+    title: 'AcitvemoodCategory', 
+    sound: 'song2',
+    by: 'mixArtist',
+    url: 'http://www.soundlocationOnYouTube.com',
+    mylist: 'myActiveList',
+ }
+
+ //All Avticemood sounds
+  {
+    _id: ObjectId(will.be.auto.generated )
+    title: 'ActivemoodCategory', 
+    sound: 'sound1',
+    by: 'mixArtist',
+    url: 'http://www.soundlocationOnYouTube.com',
+    mylist: 'myActiveList',
+ }
+
+ {
+    _id: ObjectId(will.be.auto.generated )
+    title: 'ActivemoodCategory', 
+    sound: 'sound2',
+    by: 'mixArtist',
+    url: 'http://www.soundlocationOnYouTube.com',
+    mylist: 'myActiveList',
+ }
+
+
+//ALL VIDOESS OBJECTS
+ //active mood videos
+ {
+    _id: ObjectId(will.be.auto.generated )
+    title: 'AcitvemoodCategory', 
+    sound: 'Vid1',
+    by: 'mixArtist',
+    url: 'http://www.soundlocationOnYouTube.com',
+    mylist: 'myActiveList',
+ }
+
+ {
+    _id: ObjectId(will.be.auto.generated )
+    title: 'AcitvemoodCategory', 
+    sound: 'Vid2',
+    by: 'mixArtist',
+    url: 'http://www.soundlocationOnYouTube.com',
+    mylist: 'myActiveList',
+ }
+
+ //night mood videos
+ {
+    _id: ObjectId(will.be.auto.generated )
+    title: 'NightoodCategory', 
+    sound: 'Vid1',
+    by: 'mixArtist',
+    url: 'http://www.soundlocationOnYouTube.com',
+    mylist: 'myActiveList',
+ }
+
+ {
+    _id: ObjectId(will.be.auto.generated )
+    title: 'NightmoodCategory', 
+    sound: 'Vid2',
+    by: 'mixArtist',
+    url: 'http://www.soundlocationOnYouTube.com',
+    mylist: 'myActiveList',
+ }
