df8f45985dd70bab633c78f369dbeef2e6e36773
Taylor Stemple <taylorstemple@gmail.com>
2018-01-31 09:54:56 -0800

    wednesday

--- scripts/models/soundmood.js
+++ scripts/models/soundmood.js
@@ -143,7 +143,7 @@ var __API_URL__ = 'http://localhost:3000';
     soundmood.createPlayer = function(ctx,next) {
         player1 = new YT.Player('player1', {
                               
-            videoId: `${Video.all[2].uri}`,
+            videoId: `${Video.all[3].uri}`,
             playerVars: { 'playlist': '668nUCeBHyY', 'rel': 0, 'showinfo': 0, 'loop': 1, 'autoplay': 1, 'controls': 0 },
             events: {
             'onReady': onPlayer1Ready,
@@ -160,7 +160,7 @@ var __API_URL__ = 'http://localhost:3000';
                               }
                           });
         player3 = new YT.Player('player3', {
-                              videoId: `${Song.all[5].uri}`,
+                              videoId: `${Song.all[6].uri}`,
                               playerVars: { 'rel': 0, 'showinfo': 0, 'loop': 1, 'autoplay': 1, 'controls': 0 },
                               events: {
                                 'onReady': onPlayer3Ready,
